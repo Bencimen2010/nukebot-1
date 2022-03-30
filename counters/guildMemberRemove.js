44ebd42542c5f1e5a4048e90e228eafde8f46312
Bencimen <100087979+Bencimen@users.noreply.github.com>
2022-03-30 17:08:58 +0200

    Update guildMemberRemove.js

--- counters/guildMemberRemove.js
+++ counters/guildMemberRemove.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 module.exports = async (bot) => {
     bot.on("guildMemberRemove", async guildMember => {
-        bot.channels.cache.get("950419941545353277").send(`${guildMember} kilépett a szerveről!`)
+        bot.channels.cache.get("950419941545353277").send(`<@${guildMember.id}> kilépett a szerveről!`)
     })
 }
