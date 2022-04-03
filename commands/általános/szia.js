1bacb5df004ab0766bf7fffb71d909e55f07d06d
Bencimen <100087979+Bencimen@users.noreply.github.com>
2022-04-03 18:20:13 +0200

    Delete commands/általános directory

--- commands/általános/szia.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-module.exports = {
-    name: "szia",
-    category: "Általános",
-    description: "A bot köszön neked!",
-    run: async (bot, message, args) => {
-        message.reply("Szia, üdvözöllek a NukeMC discord szerverén!")
-    }
-}
