4190ff6e6e57db2d4a9b19355399c0eb249d10df
Bencimen <100087979+Bencimen@users.noreply.github.com>
2022-04-04 14:14:53 +0200

    Update guildMemberAdd.js

--- counters/guildMemberAdd.js
+++ counters/guildMemberAdd.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 module.exports = async (bot) => {
     bot.on("guildMemberAdd", async guildMember => {
-        bot.channels.cache.get("950419910964703333").send(`<@${guildMember.id}> csatlakozott a NukeMC [1.16.5]-hez!`)
+        bot.channels.cache.get("950419910964703333").send(`<@${guildMember.id}> csatlakozott a NukeMC [1.16.5]-höz!`)
     })
 }
